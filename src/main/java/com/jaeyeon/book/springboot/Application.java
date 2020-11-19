3d9e699da80094a4930bc9eaa8f9161ca2246e23
jaeyeon <jae518@naver.com>
2020-11-20 04:07:08 +0900

    EC2 - S3 연동

--- src/main/java/com/jaeyeon/book/springboot/Application.java
+++ src/main/java/com/jaeyeon/book/springboot/Application.java
@@ -18,49 +18,5 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public CommandLineRunner mappingDemo(ProductRepository productRepository) {
-        return args -> {
-            Product p = new Product();
-            p.setName("예쁜 바지");
-            p.setProductImg("pants-2.png");
-            p.setCategory("Outer");
-            p.setDescription("제품 상세 설명");
-            p.setPrice(500000);
-            p.setDiscount(30);
-
-            productRepository.save(p);
-
-            Product a = new Product();
-            a.setName("예쁜 바지들");
-            a.setProductImg("pants-1.png");
-            a.setCategory("Outer");
-            a.setDescription("제품 상세 설명");
-            a.setPrice(180000);
-            a.setDiscount(30);
-
-            productRepository.save(a);
-
-            Product v = new Product();
-            v.setName("예쁜 코트");
-            v.setProductImg("clothing-2.png");
-            v.setCategory("Outer");
-            v.setDescription("제품 상세 설명");
-            v.setPrice(230000);
-            v.setDiscount(30);
-
-            productRepository.save(v);
-
-            Product q = new Product();
-            q.setName("예쁜 상의");
-            q.setProductImg("clothing-2.png");
-            q.setCategory("Outer");
-            q.setDescription("제품 상세 설명");
-            q.setPrice(230000);
-            q.setDiscount(30);
-
-            productRepository.save(q);
-        };
-    }
 }
 
