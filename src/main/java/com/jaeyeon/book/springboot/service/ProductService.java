e52eca053dd6ad8adb1cd9265be689717f26190a
jaeyeon <jae518@naver.com>
2020-11-20 15:37:19 +0900

    Product 도메인 - productImg 컬럼 -> 파일 업로드를 로컬에서 AWS S3로 변경

--- src/main/java/com/jaeyeon/book/springboot/service/ProductService.java
+++ src/main/java/com/jaeyeon/book/springboot/service/ProductService.java
@@ -36,15 +36,13 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    private final Path rootLocation;
+//    private final Path rootLocation;
 
-    private String productImgName = "";
-
-    @Autowired
-    public ProductService(FileUploadProperties prop) {
-        this.rootLocation = Paths.get(prop.getProductUploadDir())
-                .toAbsolutePath().normalize();
-    }
+//    @Autowired
+//    public ProductService(FileUploadProperties prop) {
+//        this.rootLocation = Paths.get(prop.getProductUploadDir())
+//                .toAbsolutePath().normalize();
+//    }
 
     @Transactional
     public Long save(ProductRequestDto productRequestDto) {
@@ -55,8 +53,8 @@ public class ProductService {
         product.setCategory(productRequestDto.getCategory());
         product.setDescription(productRequestDto.getDescription());
         product.setDiscount(productRequestDto.getDiscount());
-        product.setPrice(productRequestDto.getPrice());
-        product.setProductImg(productImgName);
+        product.setPrice(productRequestDto.getPrice());;
+        product.setProductImg(productRequestDto.getProductImg());
         product.setProductStatus(ProductStatus.SALE);
 
         return productRepository.save(product).getId();
@@ -73,61 +71,61 @@ public class ProductService {
         product.setDescription(productRequestDto.getDescription());
         product.setDiscount(productRequestDto.getDiscount());
         product.setPrice(productRequestDto.getPrice());
-        product.setProductImg(productImgName);
+        product.setProductImg(productRequestDto.getProductImg());
         product.setProductStatus(ProductStatus.SALE);
 
         return productRepository.save(product).getId();
     }
 
-    @Transactional
-    public UploadFile uploadProductImage(MultipartFile file) throws Exception {
-        try {
-            if (file.isEmpty()) {
-                throw new Exception("Failed to store empty file " + file.getOriginalFilename());
-            }
+//    @Transactional
+//    public UploadFile uploadProductImage(MultipartFile file) throws Exception {
+//        try {
+//            if (file.isEmpty()) {
+//                throw new Exception("Failed to store empty file " + file.getOriginalFilename());
+//            }
+//
+//            String saveFileName = UploadFileUtil.fileSave(rootLocation.toString(), file);
+//
+//            if (saveFileName.toCharArray()[0] == '/') {
+//                saveFileName = saveFileName.substring(1);
+//            }
+//
+//            Resource resource = loadAsResource(saveFileName);
+//            productImgName = saveFileName;
+//            UploadFile saveFile = new UploadFile();
+//            saveFile.setSaveFileName(saveFileName);
+//            saveFile.setFileName(file.getOriginalFilename());
+//            saveFile.setContentType(file.getContentType());
+//            saveFile.setFilePath(rootLocation.toString().replace(File.separatorChar, '/') + File.separator + saveFileName);
+//            saveFile.setSize(resource.contentLength());
+//
+//            return saveFile;
+//        } catch (IOException e) {
+//            throw new Exception("Failed to store file " + file.getOriginalFilename(), e);
+//        }
+//    }
 
-            String saveFileName = UploadFileUtil.fileSave(rootLocation.toString(), file);
+//    private Resource loadAsResource(String fileName) throws Exception {
+//        try {
+//            if (fileName.toCharArray()[0] == '/') {
+//                fileName = fileName.substring(1);
+//            }
+//
+//            Path file = loadPath(fileName);
+//            Resource resource = new UrlResource(file.toUri());
+//            if (resource.exists() || resource.isReadable()) {
+//                return resource;
+//            } else {
+//                throw new Exception("Could not read file: " + fileName);
+//            }
+//        } catch (Exception e) {
+//            throw new Exception("Could not read file: " + fileName);
+//        }
+//    }
 
-            if (saveFileName.toCharArray()[0] == '/') {
-                saveFileName = saveFileName.substring(1);
-            }
-
-            Resource resource = loadAsResource(saveFileName);
-            productImgName = saveFileName;
-            UploadFile saveFile = new UploadFile();
-            saveFile.setSaveFileName(saveFileName);
-            saveFile.setFileName(file.getOriginalFilename());
-            saveFile.setContentType(file.getContentType());
-            saveFile.setFilePath(rootLocation.toString().replace(File.separatorChar, '/') + File.separator + saveFileName);
-            saveFile.setSize(resource.contentLength());
-
-            return saveFile;
-        } catch (IOException e) {
-            throw new Exception("Failed to store file " + file.getOriginalFilename(), e);
-        }
-    }
-
-    private Resource loadAsResource(String fileName) throws Exception {
-        try {
-            if (fileName.toCharArray()[0] == '/') {
-                fileName = fileName.substring(1);
-            }
-
-            Path file = loadPath(fileName);
-            Resource resource = new UrlResource(file.toUri());
-            if (resource.exists() || resource.isReadable()) {
-                return resource;
-            } else {
-                throw new Exception("Could not read file: " + fileName);
-            }
-        } catch (Exception e) {
-            throw new Exception("Could not read file: " + fileName);
-        }
-    }
-
-    private Path loadPath(String fileName) {
-        return rootLocation.resolve(fileName);
-    }
+//    private Path loadPath(String fileName) {
+//        return rootLocation.resolve(fileName);
+//    }
 
     @Transactional
     public void delete (Long id) {
